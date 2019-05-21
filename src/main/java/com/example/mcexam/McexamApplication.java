78830d8f6b8a7a53f8bd3af6447d46b91395b048
DESKTOP-H6SJVQ1\Martin <mart963g@stud.kea.dk>
2019-05-21 14:30:52 +0300

    add test data

--- src/main/java/com/example/mcexam/McexamApplication.java
+++ src/main/java/com/example/mcexam/McexamApplication.java
@@ -1,7 +1,11 @@
 package com.example.mcexam;
 
+import com.example.mcexam.model.Accessory;
 import com.example.mcexam.model.Burger;
+import com.example.mcexam.model.Drink;
+import com.example.mcexam.repository.AccessoryRepository;
 import com.example.mcexam.repository.BurgerRepository;
+import com.example.mcexam.repository.DrinkRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +19,24 @@ public class McexamApplication {
     }
 
     @Bean
-    public CommandLineRunner fillTestData(BurgerRepository burgerRepository)
+    public CommandLineRunner fillTestData(BurgerRepository burgerRepository, DrinkRepository drinkRepository,
+                                          AccessoryRepository accessoryRepository)
     {
         return (args) -> {
+            //Burgers
             burgerRepository.save(new Burger("BigMac",19.99));
+            burgerRepository.save(new Burger("Cheeseburger",10.00));
+            burgerRepository.save(new Burger("McBacon",25.00));
+
+            //Drinks
+            drinkRepository.save(new Drink("Coke","Medium",15.00));
+            drinkRepository.save(new Drink("Sprite","Large", 20.00));
+            drinkRepository.save(new Drink("Strawberry Milkshake","Small",14.99));
+
+            //Accessories
+            accessoryRepository.save(new Accessory("Fries","Small",14.99));
+            accessoryRepository.save(new Accessory("Curly fries","Big", 20.00));
+            accessoryRepository.save(new Accessory("Chilli cheese tops","Big",24.99));
         };
     }
 
